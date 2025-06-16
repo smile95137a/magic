5410f788e46d2664f48b05760d75c141105811a9
jim <>
2025-06-16 10:52:23 +0800

    no message

--- src/components/GodList.vue
+++ src/components/GodList.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="god">
     <div class="god__list">
-      <div v-for="(god, index) in gods" :key="index" class="god__card">
+      <div v-for="(god, key) in godImages" :key="key" class="god__card">
         <div class="god__content">
-          <h2 class="god__title">{{ god.name }}</h2>
+          <img :src="god.default" :alt="god.label" class="god__image" />
+          <Title :text="god.label" color="dark" />
           <p class="god__description" v-html="god.description"></p>
-          <img :src="god.image" :alt="god.name" class="god__image" />
         </div>
         <div class="god__action">
-          <button class="god__action-btn">
-            <span class="god__action-btn-text">請神保庇</span>
-          </button>
+          <StartButton styleType="red" label="請神保庇" />
         </div>
       </div>
     </div>
@@ -18,39 +16,9 @@
 </template>
 
 <script setup>
-import godYuelao from '@/assets/image/god_yuelao.png';
-import godCaishen from '@/assets/image/god_yuelao.png';
-import godWenchang from '@/assets/image/god_yuelao.png';
-import godZhongkui from '@/assets/image/god_yuelao.png';
-import godGuangong from '@/assets/image/god_yuelao.png';
-
-const gods = [
-  {
-    name: '月老',
-    image: godYuelao,
-    description: '“緣份到了他人,緣定莫失情緣”<br>（補上說明內容）',
-  },
-  {
-    name: '財神',
-    image: godCaishen,
-    description: '“招財進寶,生意興隆”<br>（補上說明內容）',
-  },
-  {
-    name: '文昌',
-    image: godWenchang,
-    description: '“掌管考試文運的聖神”<br>（補上說明內容）',
-  },
-  {
-    name: '鐘馗',
-    image: godZhongkui,
-    description: '“鎮煞辟邪,掃蕩妖魔”<br>（補上說明內容）',
-  },
-  {
-    name: '關公',
-    image: godGuangong,
-    description: '“忠義精神,守財辟邪”<br>（補上說明內容）',
-  },
-];
+import godImages from '@/data/godImages';
+import Title from '@/components/common/Title.vue';
+import StartButton from '@/components/StartButton.vue';
 </script>
 
 <style lang="scss" scoped>
@@ -80,28 +48,30 @@ const gods = [
   }
 
   &__content {
-    flex: 1;
-  }
+    position: relative;
 
-  &__title {
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 1rem;
-  }
+    .god__image {
+      float: right;
+      width: 204px;
+      height: 204px;
+      object-fit: contain;
+      margin-left: 1rem;
+      margin-bottom: 0.5rem;
 
-  &__description {
-    font-size: 14px;
-    line-height: 1.6;
-    color: #222;
-    margin-bottom: 1rem;
-  }
+      @media (max-width: 768px) {
+        float: none;
+        display: block;
+        margin: 0 auto 1rem;
+      }
+    }
 
-  &__image {
-    width: 120px;
-    flex-shrink: 0;
-
-    @media (max-width: 768px) {
-      width: 100px;
+    .god__description {
+      position: relative;
+      margin-top: 24px;
+      font-size: 14px;
+      line-height: 1.6;
+      color: #222;
+      text-align: justify;
     }
   }
 
